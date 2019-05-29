bf948c4b956580eaca8442a1e9f84738b1695f76
Kawaiisun <kawaiisungames@gmail.com>
2019-05-29 14:33:16 -0500

    weapon sway
    made the weapon lag behind the player's camera movements to give the controller more of a fluid design

---
+++ Simple Hostile/Assets/Scripts/Sway.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.Kawaiisun.SimpleHostile
+{
+    public class Sway : MonoBehaviour
+    {
+        #region Variables
+
+        public float intensity;
+        public float smooth;
+
+        private Quaternion origin_rotation;
+
+        #endregion
+
+
+
+        #region MonoBehaviour Callbacks
+
+        private void Start()
+        {
+            origin_rotation = transform.localRotation;
+        }
+
+        private void Update()
+        {
+            UpdateSway();
+        }
+
+        #endregion
+
+
+
+        #region Private Methods
+
+        private void UpdateSway ()
+        {
+            //controls
+            float t_x_mouse = Input.GetAxis("Mouse X");
+            float t_y_mouse = Input.GetAxis("Mouse Y");
+
+            //calculate target rotation
+            Quaternion t_x_adj = Quaternion.AngleAxis(-intensity * t_x_mouse, Vector3.up);
+            Quaternion t_y_adj = Quaternion.AngleAxis(intensity * t_y_mouse, Vector3.right);
+            Quaternion target_rotation = origin_rotation * t_x_adj * t_y_adj;
+
+            //rotate towards target rotation
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, target_rotation, Time.deltaTime * smooth);
+        }
+
+        #endregion
+    }
+}
