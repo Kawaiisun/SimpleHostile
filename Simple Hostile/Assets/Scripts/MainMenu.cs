2e97468a44f2e12daff664f484cc53acbf747edc
Kawaiisun <kawaiisungames@gmail.com>
2019-08-05 22:22:47 -0500

    main menu

---
+++ Simple Hostile/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.Kawaiisun.SimpleHostile
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public Launcher launcher;
+
+        public void JoinMatch()
+        {
+            launcher.Join();
+        }
+
+        public void CreateMatch()
+        {
+            launcher.Create();
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
