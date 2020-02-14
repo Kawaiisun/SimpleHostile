f127f1aeb31b88bbc66617644d3148ea463f00fd
Kawaiisun <kawaiisungames@gmail.com>
2020-02-14 14:26:52 -0600

    room list

--- Simple Hostile/Assets/Scripts/Launcher.cs
+++ Simple Hostile/Assets/Scripts/Launcher.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine.UI;
 
 namespace Com.Kawaiisun.SimpleHostile
@@ -18,6 +19,13 @@ namespace Com.Kawaiisun.SimpleHostile
         public InputField usernameField;
         public static ProfileData myProfile = new ProfileData();
 
+        public GameObject tabMain;
+        public GameObject tabRooms;
+
+        public GameObject buttonRoom;
+
+        private List<RoomInfo> roomList;
+
         public void Awake()
         {
             PhotonNetwork.AutomaticallySyncScene = true;
@@ -28,6 +36,7 @@ namespace Com.Kawaiisun.SimpleHostile
         {
             Debug.Log("CONNECTED!");
 
+            PhotonNetwork.JoinLobby();
             base.OnConnectedToMaster();
         }
 
@@ -59,7 +68,60 @@ namespace Com.Kawaiisun.SimpleHostile
 
         public void Create ()
         {
-            PhotonNetwork.CreateRoom("");
+            RoomOptions options = new RoomOptions(); 
+            options.MaxPlayers = 8;
+
+            PhotonNetwork.CreateRoom("", options);
+        }
+
+        public void TabCloseAll()
+        {
+            tabMain.SetActive(false);
+            tabRooms.SetActive(false);
+        }
+
+        public void TabOpenMain ()
+        {
+            TabCloseAll();
+            tabMain.SetActive(true);
+        }
+
+        public void TabOpenRooms ()
+        {
+            TabCloseAll();
+            tabRooms.SetActive(true);
+        }
+
+        private void ClearRoomList ()
+        {
+            Transform content = tabRooms.transform.Find("Scroll View/Viewport/Content");
+            foreach (Transform a in content) Destroy(a.gameObject);
+        }
+
+        public override void OnRoomListUpdate(List<RoomInfo> p_list)
+        {
+            roomList = p_list;
+            ClearRoomList();
+
+            Transform content = tabRooms.transform.Find("Scroll View/Viewport/Content");
+
+            foreach (RoomInfo a in roomList)
+            {
+                GameObject newRoomButton = Instantiate(buttonRoom, content) as GameObject;
+
+                newRoomButton.transform.Find("Name").GetComponent<Text>().text = a.Name;
+                newRoomButton.transform.Find("Players").GetComponent<Text>().text = a.PlayerCount + " / " + a.MaxPlayers;
+
+                newRoomButton.GetComponent<Button>().onClick.AddListener(delegate { JoinRoom(newRoomButton.transform); });
+            }
+
+            base.OnRoomListUpdate(roomList);
+        }
+
+        public void JoinRoom (Transform p_button)
+        {
+            string t_roomName = p_button.Find("Name").GetComponent<Text>().text;
+            PhotonNetwork.JoinRoom(t_roomName);
         }
 
         public void StartGame ()
